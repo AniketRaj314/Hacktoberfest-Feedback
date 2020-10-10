a4bbece857553aa0cd294004e56e669ed3776e01
Prasanjit-4 <69084219+Prasanjit-4@users.noreply.github.com>
2020-10-10 20:27:01 +0530

    Update template.py

--- template.py
+++ template.py
@@ -1,3 +1,3 @@
-# Prasanjit Panda
-# HF93580
-print("Thanks to HacktoberFest Mumbai team and Digital Ocean for organizing this event \nGreat way to start my open source journey")
+# your name
+# your id
+print(" your feedback")
