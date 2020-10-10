b6844c2db26e6c597704d3bbbe22373a3b62e290
Prasanjit-4 <69084219+Prasanjit-4@users.noreply.github.com>
2020-10-10 20:25:13 +0530

    Added my name , id and feedback

--- template.py
+++ template.py
@@ -1,3 +1,3 @@
-# Your Name
-# Your ID
-print("Enter feedback between the quotes!")
+# Prasanjit Panda
+# HF93580
+print("Thanks to HacktoberFest Mumbai team and Digital Ocean for organizing this event \nGreat way to start my open source journey")
